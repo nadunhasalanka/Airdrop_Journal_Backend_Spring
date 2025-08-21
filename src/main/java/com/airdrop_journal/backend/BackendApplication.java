6f5a08adbb0fbfe8d1b5f7e5f4d004f855fa24bc
nadunhasalanka <nadun202327@gmail.com>
2025-08-21 15:03:42 +0530

    initial commit

---
+++ src/main/java/com/airdrop_journal/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.airdrop_journal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
